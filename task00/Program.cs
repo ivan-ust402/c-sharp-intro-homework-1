6630da8a3d35901e79c300262cc25c611101ecf1
Roman <roman@MacBook-Pro-Roman.local>
2022-09-20 13:51:36 +0700

    First task was complited

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 2
 // Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше 
 // a = 5, b = 7 => max = 7, min = 5
+Console.WriteLine(" ");
+Console.WriteLine("Вы находитесь в программе, вычисляющей большее из двух введенных числел. Нажмите Enter, чтобы продолжить...");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) {
+    Console.WriteLine($"max = {a}, min = {b}");
+} else {
+    Console.WriteLine($"max = {b}, min = {a}");
+}
