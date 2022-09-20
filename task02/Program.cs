1e4348dec258e1806f3749ad3397c0b04a57d15e
Roman <roman@MacBook-Pro-Roman.local>
2022-09-20 14:31:28 +0700

    Third programm was complited

---
+++ task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6
+// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится на два без остатка)
+// 4 -> да 
+// -3 -> нет 
+Console.Clear();
+Console.WriteLine("Вы находитесь в программе, определяющей четность числа. Нажмите Enter, чтобы продолжить...");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0) {
+    Console.WriteLine($"Число {a} - четное");
+} else {
+    Console.WriteLine($"Число {a} - нечетное");
+}
